d819b3b9a98f90715e10b6bb9f1770867f0096f6
Adrian Valdes <adrianvaldeshdz@gmail.com>
2021-04-03 14:48:10 +0200

    add exercises

--- curry.js
+++ curry.js
@@ -16,17 +16,39 @@ const incrementBy2 = curriedAdd(2);
 const mult = (x, y) => x * y;
 const curriedMult = curry(mult);
 const multBy2 = curriedMult(2);
-
-console.log(multBy2(4));
+// console.log(multBy2(4));
 
 //modulo
 const modulo = curry((x, y) => y % x);
 
 const isOdd = modulo(2);
-
-console.log(isOdd(2)); // 0 => false; 1 => true
+// console.log(isOdd(2)); // 0 => false; 1 => true
 
 //filter
 const filter = curry((f, xs) => xs.filter(f));
 const getOdds = filter(isOdd);
-console.log(getOdds([1, 2, 3, 4, 5, 6, 7]));
+// console.log(getOdds([1, 2, 3, 4, 5, 6, 7]));
+
+// Exercise 1 - words
+//==============
+const split = curry((delimiter, string) => string.split(delimiter));
+const words = split(' ');
+// console.log(words('Jingle bells Batman smells'));
+
+// Exercise 1a
+//==============
+//use map to make a new words fn that not only works on 1 string, but on an array of strings.
+
+const map = curry((fn, xs) => xs.map(fn));
+const sentences = map(words);
+// console.log(sentences(['Jingle bells Batman smells', 'Robin laid an egg']));
+
+// Exercise 3
+//==============
+// Use the helper function keepHighest to refactor max
+
+const keepHighest = (x, y) => (x >= y ? x : y); // <- leave be
+
+const reduce = curry((fn, xs) => xs.reduce(fn));
+const max = reduce(keepHighest);
+console.log(max([323, 523, 554, 123, 5234]));
