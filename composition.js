906df1b2cd8c2e47a669e97221de28eb01a10976
Adrian Valdes <adrianvaldeshdz@gmail.com>
2021-04-04 21:23:28 +0200

    add ramda

--- composition.js
+++ composition.js
@@ -1,7 +1,10 @@
+const { compose } = require('ramda');
+
 const toUpper = (str) => str.toUpperCase();
 const exclaim = (str) => str + '!';
 const first = (xs) => xs[0];
 
-const compose = (f, g) => (x) => f(g(x));
+const basicCompose = (f, g) => (x) => f(g(x));
+
 const shout = compose(exclaim, toUpper);
 console.log(shout('books'));
