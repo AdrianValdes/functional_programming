4379e7ac7d8a888177addd7f94b06f288cbeca0d
Adrian Valdes <adrianvaldeshdz@gmail.com>
2021-04-05 13:03:05 +0200

    add functors exercises

--- functors2.js
+++ functors2.js
@@ -15,4 +15,53 @@ const halfTheFirstLargeNumber = (xs) =>
     .fold((answer) => `The answer is ${answer}`);
 
 const res = halfTheFirstLargeNumber([1, 4, 50]);
-console.log(res);
+// console.log(res);
+
+// Ex1: Using Box, refactor moneyToFloat to be unnested.
+// =========================
+const moneyToFloat_ = (str) => parseFloat(str.replace(/\$/, ''));
+
+const moneyToFloat = (str) =>
+  Box(str)
+    .map((str) => str.replace(/\$/, ''))
+    .fold(parseFloat);
+
+// console.log(moneyToFloat('$5.00')); //5
+
+// Ex2: Using Box, refactor percentToFloat to remove assignment
+// =========================
+const percentToFloat_ = (str) => {
+  const float = parseFloat(str.replace(/\%/, ''));
+  return float * 0.01;
+};
+
+const percentToFloat = (str) =>
+  Box(str)
+    .map((str) => str.replace(/\%/, ''))
+    .map(parseFloat)
+    .fold((number) => number * 0.01);
+
+// console.log(percentToFloat('20%')); // 0.2
+
+// Ex3: Using Box, refactor applyDiscount (hint: each variable introduces a new Box)
+// =========================
+const applyDiscount_ = (price, discount) => {
+  const cents = moneyToFloat(price);
+  const savings = percentToFloat(discount);
+  return cents - cents * savings;
+};
+
+const applyDiscount = (price, discount) =>
+  Box(price)
+    .map(moneyToFloat)
+    .fold((cents) =>
+      Box(discount)
+        .map(percentToFloat)
+        .fold((savings) => cents - cents * savings)
+    );
+
+const applyDiscount1 = (price, discount) =>
+  Box(moneyToFloat(price)).fold((cents) =>
+    Box(percentToFloat(discount)).fold((savings) => cents - cents * savings)
+  );
+console.log(applyDiscount1('$5.00', '20%'));
