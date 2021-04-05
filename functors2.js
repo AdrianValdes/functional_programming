4dd952ec34a9da1f5ec10be953346ce96afc3b57
Adrian Valdes <adrianvaldeshdz@gmail.com>
2021-04-05 12:39:59 +0200

    add functor example

---
+++ functors2.js
@@ -0,0 +1,18 @@
+const Box = require('./box');
+
+const first = (xs) => xs[0];
+
+const halfTheFirstLargeNumber_ = (xs) => {
+  const found = xs.filter((x) => x >= 20);
+  const answer = first(found) / 2;
+  return `The answer is ${answer}`;
+};
+
+const halfTheFirstLargeNumber = (xs) =>
+  Box(xs)
+    .map((xs) => xs.filter((x) => x >= 20))
+    .map((found) => first(found) / 2)
+    .fold((answer) => `The answer is ${answer}`);
+
+const res = halfTheFirstLargeNumber([1, 4, 50]);
+console.log(res);
