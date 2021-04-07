18a25b8e9e861c1b5974e377c22daac53abc6c29
Adrian Valdes <adrianvaldeshdz@gmail.com>
2021-04-07 13:25:16 +0200

    add fromNullable function

--- either.js
+++ either.js
@@ -14,13 +14,18 @@ const Left = (x) => ({
   toString: `Left(${x})`,
 });
 
+const fromNullable = (x) => (x != null ? Right(x) : Left(null));
+
 const findColor_ = (name) =>
   ({ red: '#ff4444', blue: '#3b5998', yellow: '#fff68f' }[name]);
 
-const findColor = (name) => {
+const findColor_0 = (name) => {
   const found = { red: '#ff4444', blue: '#3b5998', yellow: '#fff68f' }[name];
   return found ? Right(found) : Left('missing');
 };
+
+const findColor = (name) =>
+  fromNullable({ red: '#ff4444', blue: '#3b5998', yellow: '#fff68f' }[name]);
 
 const res = () =>
   findColor('red')
